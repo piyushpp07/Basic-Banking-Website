da24dda08aea9a113138f6e250295651a2b36a40
piyushpp07 <59327328+piyushpp07@users.noreply.github.com>
2021-08-11 23:18:17 +0530

    done for todey

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { db } from "../firebase";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Form } from 'react-bootstrap';
